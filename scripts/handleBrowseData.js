ac5732ec59991457371f70447bf67b13dc331227
Eghosa Ordia <eghodia1004@yahoo.com>
2026-01-02 11:18:53 +0100

    feat: add model information to BIOS boards and enhance search results display with improved styling

--- scripts/handleBrowseData.js
+++ scripts/handleBrowseData.js
@@ -13,17 +13,50 @@ const searchResults = document.getElementById("searchResults");
 
 let debounceTimer;
 
+const renderResults = (results) => {
+  searchResults.innerHTML = results
+    .map((entry) => {
+      return `<div class="search__result">
+            <div>
+              <span class="search__title">Board Number: </span>
+              <span class="search__value">${entry.boardNumber}</span>
+            </div>
+            <div>
+              <span class="search__title">Board Title: </span>
+              <span class="search__value"
+                >${entry.boardTitle}</span
+              >
+            </div>
+            <div>
+              <span class="search__title">Model:</span>
+              <span class="search__value">${entry.model}</span>
+            </div>
+          </div>`;
+    })
+    .join("");
+  console.log(results);
+
+  searchResults.classList.toggle("active", results.length > 0);
+};
+
 searchInput.addEventListener("input", function () {
   clearTimeout(debounceTimer);
+  const query = searchInput.value.trim().toLowerCase();
+
+  if (!query) {
+    searchResults.classList.remove("active");
+    return;
+  }
 
   debounceTimer = setTimeout(async () => {
-    if (searchInput.value.trim() == "") {
-      searchResults.classList.remove("active");
-      return;
-    }
-
-    searchResults.classList.add("active");
-
     const allData = await fetchData();
-  }, 600);
+    const filtered = allData.filter(
+      (item) =>
+        item.boardNumber.toLowerCase().includes(query) ||
+        item.boardTitle.toLowerCase().includes(query) ||
+        item.model.toLowerCase().includes(query)
+    );
+
+    renderResults(filtered);
+  }, 400);
 });
